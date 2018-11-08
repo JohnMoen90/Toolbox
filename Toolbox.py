556de702688913bb86a0e52e80707aa3df08e235
JohnMoen90 <uk3718vi@go.minneapolis.edu>
2018-11-08 11:31:47 -0600

    First function added

--- Toolbox.py
+++ Toolbox.py
@@ -3,8 +3,10 @@ A collection of functions
 - John Moen
 """
 import sys
+import datetime
 
 
+# Offers option to restart program
 def restart():
     """ This function should be used at the end of a while loop to prompt user to restart
     :return: N/A """
@@ -18,5 +20,27 @@ def restart():
         else:
             break
 
+
+# Returns morning/afternoon/evening depending on time of day
+def time_day():
+    """
+    This function simply returns the correct time of day word when the function is run
+    :return: morning, evening, or afternoon depending on time of day
+    """
+
+    # Get the time
+    time = str(datetime.datetime.now().time())
+
+    # Slice the hour out
+    time = int(time[:2])
+
+    # Get appropriate time of day word
+    if time < 12:
+        day_time = "morning"
+    elif time >= 17:
+        day_time = "evening"
+    else:
+        day_time = "afternoon"
+    return day_time
 
 
